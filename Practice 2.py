07ca5a2934ab94d66b225de9bca26d5469693c9c
Piya-JetLearner <piyajhp@gmail.com>
2026-03-06 19:52:00 +0000

    Add files via upload

---
+++ Practice 2.py
@@ -0,0 +1,25 @@
+"""
+Write a program that will "merge" the two dictionaries (d1 and d2).
+d1 = {'Adam Smith': 'A', 'Judy Paxton': 'B+'}
+d2 = {'Mary Louis': 'A', 'Patrick White': 'C'} 
+"""
+d1 = {'Adam Smith': 'A', 'Judy Paxton': 'B+'}
+d2 = {'Mary Louis': 'A', 'Patrick White': 'C'} 
+
+d1.update(d2)
+print(d1)
+
+"""
+2.  Write a program to Match key values in two dictionaries
+Sample Output
+A = {'Tamil' : 92, 'English' : 56, 'Maths' : 88, 'Science' : 97, 'Social' : 89}
+B= {'Tamil' : 78, 'English' : 68, 'Maths' : 88, 'Science' : 97, 'Social' : 56}
+Maths : 88 is present in both A and B
+"""
+A = {'Tamil' : 92, 'English' : 56, 'Maths' : 88, 'Science' : 97, 'Social' : 89}
+B= {'Tamil' : 78, 'English' : 68, 'Maths' : 88, 'Science' : 97, 'Social' : 56}
+for key in A.keys():
+    if key in B:
+        if A[key] == B[key]:
+            print (key, ':',A[key],"is present in both A and B")
+
